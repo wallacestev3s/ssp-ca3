e3244214f338084e56a21b8615aeb8fecd843b0b
wallacestev3s <wallace_esteves@hotmail.com>
2022-01-04 23:21:47 +0000

    fixing index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http'), // this module provides the HTTP server
       path = require('path'), // allows us to work with files and directory paths
       express = require('express'),// allow the the app to respond to http requests
       fs =require('fs'), // this module allow us to work with the file system
-      xmlParse = require('xsltParse').xmlParse,// wors with xml files
+      xmlParse = require('xslt-processor').xmlParse,// wors with xml files
       xsltProcess = require('xslt-processor').xsltProcess,// this module utilize XSl transformations,
       xml2js = require('xml2js');// this module does xml to JSON conversion
 
@@ -17,7 +17,7 @@ const router = express(),     //creating an instance through express
           xsl = fs.readFileSync('WallyForge.xsl','utf8');
       let doc = xmlParse(xml),
             stylesheet = xmlParse(xsl);
-      let result = xsltProcessor(doc,stylesheet);      
+      let result = xsltProcess(doc,stylesheet);      
 
       res.end(result.toString());
  });
